941b953a6ec784e0f66c2bf7f63a23ad73021027
senseiDavo <159715555+senseiDavo@users.noreply.github.com>
2024-07-12 22:13:23 +0100

    Snake Room Start

--- Assets/Scripts/AI/Constrictor Dash Hit Box1.cs
+++ Assets/Scripts/AI/Constrictor Dash Hit Box1.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnakeDashHitBox : MonoBehaviour
+public class ConstrictorDashHitBox : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
-            other.GetComponent<Rigidbody>().velocity += transform.forward * 2 + transform.up * 1;
+            Destroy(transform.parent.gameObject);
         }
     }
 }
